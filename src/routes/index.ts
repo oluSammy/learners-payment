4d9f8962022e77dfc76e4b893942f836080d59fb
samuel olumorin <olumorinsammy@gmail.com>
2022-01-08 11:07:40 +0100

    env setup

---
+++ src/routes/index.ts
@@ -0,0 +1,10 @@
+import express, { NextFunction, Request, Response } from "express";
+
+const router = express.Router();
+
+/* GET home page. */
+router.get("/", function (req: Request, res: Response, next: NextFunction) {
+  res.render("index", { title: "Express" });
+});
+
+export default router;
