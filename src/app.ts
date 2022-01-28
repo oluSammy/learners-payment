d3785a929b4fd230950e12bdeb422b85f7725e72
samuel olumorin <olumorinsammy@gmail.com>
2022-01-28 12:09:08 +0100

    restore server

--- src/app.ts
+++ src/app.ts
@@ -25,7 +25,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-// connect Db
+// // // connect Db
 connectDb();
 
 app.get("/", (__, res) => {
